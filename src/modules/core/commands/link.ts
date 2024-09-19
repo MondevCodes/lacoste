4e3ea746faa1f75aba5c3853694165c18c311d4f
cauemondek <cauemondekk@gmail.com>
2024-09-19 00:18:10 -0300

    test: Add debug in prisma create user

--- src/modules/core/commands/link.ts
+++ src/modules/core/commands/link.ts
@@ -7,58 +7,58 @@ import { EmbedBuilder, type Message } from "discord.js";
 import type { Args } from "@sapphire/framework";
 
 @ApplyOptions<Command.Options>({
-	name: "vincular",
+  name: "vincular",
 })
 export class LinkCommand extends Command {
-	public override async messageRun(message: Message, args: Args) {
-		if (!message.inGuild()) {
-			this.container.logger.warn(
-				`[LinkCommand#messageRun] ${message.member?.id} tried to perform an action in a DM.`,
-			);
+  public override async messageRun(message: Message, args: Args) {
+    if (!message.inGuild()) {
+      this.container.logger.warn(
+        `[LinkCommand#messageRun] ${message.member?.id} tried to perform an action in a DM.`
+      );
 
-			return;
-		}
+      return;
+    }
 
-		const cachedGuild =
-			message.guild ??
-			(await this.container.client.guilds.fetch(message.guildId));
+    const cachedGuild =
+      message.guild ??
+      (await this.container.client.guilds.fetch(message.guildId));
 
-		const author =
-			message.member ?? (await message.guild?.members.fetch(message.author.id));
+    const author =
+      message.member ?? (await message.guild?.members.fetch(message.author.id));
 
-		const isAuthorized = this.container.utilities.discord.hasPermissionByRole({
-			category: "SECTOR",
-			checkFor: "PROMOCIONAL",
-			roles: author.roles,
-		});
+    const isAuthorized = this.container.utilities.discord.hasPermissionByRole({
+      category: "SECTOR",
+      checkFor: "PROMOCIONAL",
+      roles: author.roles,
+    });
 
-		if (!isAuthorized) {
-			this.container.logger.info(
-				`[LinkCommand#messageRun] ${message.member?.id} tried to perform an action in a DM.`,
-			);
+    if (!isAuthorized) {
+      this.container.logger.info(
+        `[LinkCommand#messageRun] ${message.member?.id} tried to perform an action in a DM.`
+      );
 
-			return;
-		}
+      return;
+    }
 
-		const memberResult = await args.pickResult("member");
-		const nickResult = await args.pickResult("string");
+    const memberResult = await args.pickResult("member");
+    const nickResult = await args.pickResult("string");
 
     this.container.logger.info(
-      `[LinkCommand#messageRun] memberResult: ${memberResult}`,
+      `[LinkCommand#messageRun] memberResult: ${memberResult}`
     );
 
-		// if (memberResult.isErrAnd(() => nickResult.isErr())) {
-		// 	await this.container.utilities.discord.sendEphemeralMessage(message, {
-		// 		content:
-		// 			"Comando inválido, use `vincular NickNoHabbo @NickNoDiscord` deve ser usado para vincular um usuário.",
-		// 		method: "reply",
-		// 	});
+    // if (memberResult.isErrAnd(() => nickResult.isErr())) {
+    // 	await this.container.utilities.discord.sendEphemeralMessage(message, {
+    // 		content:
+    // 			"Comando inválido, use `vincular NickNoHabbo @NickNoDiscord` deve ser usado para vincular um usuário.",
+    // 		method: "reply",
+    // 	});
 
-		// 	return;
-		// }
+    // 	return;
+    // }
 
     // START MEMBER WITHOUT DISCORD
-		if (memberResult.isErr()) {
+    if (memberResult.isErr()) {
       if (nickResult.isErr()) {
         await this.container.utilities.discord.sendEphemeralMessage(message, {
           content:
@@ -70,7 +70,7 @@ export class LinkCommand extends Command {
       }
 
       const profileResult = await this.container.utilities.habbo.getProfile(
-        nickResult.unwrap(),
+        nickResult.unwrap()
       );
 
       if (profileResult.isErr()) {
@@ -96,16 +96,15 @@ export class LinkCommand extends Command {
 
       if (existingUser) {
         await this.container.utilities.discord.sendEphemeralMessage(message, {
-          content:
-            `Este usuário já está vinculado sem o Discord. Caso queira vincular o Discord utilize: "vincular @NickNoDiscord ${profile.name}"`,
+          content: `Este usuário já está vinculado sem o Discord. Caso queira vincular o Discord utilize: "vincular @NickNoDiscord ${profile.name}"`,
           method: "reply",
         });
 
         return;
       } else {
         await this.container.prisma.user
-        .create({
-          data: {
+          .create({
+            data: {
               discordId: "0",
               habboId: profile.uniqueId,
               habboName: profile.name,
@@ -113,12 +112,18 @@ export class LinkCommand extends Command {
               latestPromotionRoleId: ENVIRONMENT.SECTORS_ROLES.INICIAL.id,
               latestPromotionJobId: ENVIRONMENT.JOBS_ROLES.VINCULADO.id,
             },
-        })
-        .catch(() => undefined);
+          })
+          .catch((error) => {
+            this.container.logger.info(
+              `[LinkCommand#messageRun] Error trying creating user without Discord: ${error}`
+            );
+
+            return;
+          });
       }
 
       const notificationChannel = await cachedGuild.channels.fetch(
-        ENVIRONMENT.NOTIFICATION_CHANNELS.HABBO_USERNAME_ADDED,
+        ENVIRONMENT.NOTIFICATION_CHANNELS.HABBO_USERNAME_ADDED
       );
 
       if (!notificationChannel?.isTextBased()) {
@@ -138,7 +143,7 @@ export class LinkCommand extends Command {
           { name: "Discord", value: "Ainda não vinculado", inline: true },
         ])
         .setThumbnail(
-          `https://www.habbo.com/habbo-imaging/avatarimage?figure=${profile.figureString}&size=b&gesture=std`,
+          `https://www.habbo.com/habbo-imaging/avatarimage?figure=${profile.figureString}&size=b&gesture=std`
         );
 
       await this.container.utilities.discord.sendEphemeralMessage(message, {
@@ -153,9 +158,9 @@ export class LinkCommand extends Command {
 
       await message.react("✅");
 
-			return;
+      return;
       // END USER WITHOUT DISCORD
-		}
+    }
 
     // if (memberResult.isErr()) {
     //   await this.container.utilities.discord.sendEphemeralMessage(message, {
@@ -167,126 +172,132 @@ export class LinkCommand extends Command {
     //   return;
     // }
 
-		const profileResult = await this.container.utilities.habbo.getProfile(
-			nickResult.unwrap(),
-		);
+    const profileResult = await this.container.utilities.habbo.getProfile(
+      nickResult.unwrap()
+    );
 
-		if (profileResult.isErr()) {
-			this.container.logger.info({
-				err: profileResult.unwrapErr(),
-			});
+    if (profileResult.isErr()) {
+      this.container.logger.info({
+        err: profileResult.unwrapErr(),
+      });
 
-			await this.container.utilities.discord.sendEphemeralMessage(message, {
-				content:
-					"Parece que o usuário informado não existe, verifique o nome e tente novamente.",
-				method: "reply",
-			});
-
-			return;
-		}
-
-		const member = memberResult.unwrap();
-		const profile = profileResult.unwrap();
-
-		const roles = (
-			await message.guild.members.fetch(member.id)
-		).roles.cache.map((role) => role.id);
-
-		const existingUser = await this.container.prisma.user.findUnique({
-			where: { habboId: profile.uniqueId },
-			select: { habboId: true, discordId: true, latestPromotionRoleId: true, latestPromotionJobId: true },
-		});
-
-    const existingUserDiscord = await this.container.prisma.user.findUnique({
-			where: { discordId: member.id },
-			select: { habboId: true, discordId: true, latestPromotionRoleId: true, habboName: true },
-		});
-
-    if (existingUserDiscord) {
       await this.container.utilities.discord.sendEphemeralMessage(message, {
-				content:
-					`Este perfil do Discord já está vinculado com a conta do Habbo: ${existingUserDiscord.habboName}`,
-				method: "reply",
-			});
+        content:
+          "Parece que o usuário informado não existe, verifique o nome e tente novamente.",
+        method: "reply",
+      });
 
       return;
     }
 
-		const highestJob =
-			this.container.utilities.discord.inferHighestJobRole(roles);
+    const member = memberResult.unwrap();
+    const profile = profileResult.unwrap();
 
-		const highestSector =
-			this.container.utilities.discord.inferHighestSectorRole(roles);
+    const roles = (
+      await message.guild.members.fetch(member.id)
+    ).roles.cache.map((role) => role.id);
 
-		for await (const role of ENVIRONMENT.DEFAULT_ROLES) {
-			await cachedGuild.members
-				.addRole({
-					user: member,
-					role,
-				})
-				.catch(() => undefined);
-		}
+    const existingUser = await this.container.prisma.user.findUnique({
+      where: { habboId: profile.uniqueId },
+      select: {
+        habboId: true,
+        discordId: true,
+        latestPromotionRoleId: true,
+        latestPromotionJobId: true,
+      },
+    });
+
+    const existingUserDiscord = await this.container.prisma.user.findUnique({
+      where: { discordId: member.id },
+      select: {
+        habboId: true,
+        discordId: true,
+        latestPromotionRoleId: true,
+        habboName: true,
+      },
+    });
+
+    if (existingUserDiscord) {
+      await this.container.utilities.discord.sendEphemeralMessage(message, {
+        content: `Este perfil do Discord já está vinculado com a conta do Habbo: ${existingUserDiscord.habboName}`,
+        method: "reply",
+      });
+
+      return;
+    }
+
+    const highestJob =
+      this.container.utilities.discord.inferHighestJobRole(roles);
+
+    const highestSector =
+      this.container.utilities.discord.inferHighestSectorRole(roles);
+
+    for await (const role of ENVIRONMENT.DEFAULT_ROLES) {
+      await cachedGuild.members
+        .addRole({
+          user: member,
+          role,
+        })
+        .catch(() => undefined);
+    }
 
     this.container.logger.info(
       `Sector = ${highestSector}`,
-      `Job = ${highestJob}`,
+      `Job = ${highestJob}`
     );
-		if (!existingUser?.latestPromotionRoleId) {
-			await cachedGuild.members
-				.addRole({
-					user: member,
-					role: ENVIRONMENT.SECTORS_ROLES.INICIAL.id,
-				})
-				.catch(() => undefined);
-			this.container.logger.info(
-			  "Sector Inicial role Added"
-			);
+    if (!existingUser?.latestPromotionRoleId) {
+      await cachedGuild.members
+        .addRole({
+          user: member,
+          role: ENVIRONMENT.SECTORS_ROLES.INICIAL.id,
+        })
+        .catch(() => undefined);
+      this.container.logger.info("Sector Inicial role Added");
 
-			await cachedGuild.members
-				.addRole({
-					user: member,
-					role: ENVIRONMENT.JOBS_ROLES.VINCULADO.id,
-				})
-				.catch(() => undefined);
-      this.container.logger.info(
-        "Job Vinculado role Added"
-      );
+      await cachedGuild.members
+        .addRole({
+          user: member,
+          role: ENVIRONMENT.JOBS_ROLES.VINCULADO.id,
+        })
+        .catch(() => undefined);
+      this.container.logger.info("Job Vinculado role Added");
 
       if (existingUser) {
         await this.container.prisma.user.update({
           where: {
-          habboId: profile.uniqueId,
+            habboId: profile.uniqueId,
           },
           data: {
-          latestPromotionDate: new Date(),
-          latestPromotionRoleId: ENVIRONMENT.SECTORS_ROLES.INICIAL.id,
-          latestPromotionJobId: ENVIRONMENT.JOBS_ROLES.VINCULADO.id,
+            latestPromotionDate: new Date(),
+            latestPromotionRoleId: ENVIRONMENT.SECTORS_ROLES.INICIAL.id,
+            latestPromotionJobId: ENVIRONMENT.JOBS_ROLES.VINCULADO.id,
           },
         });
 
         await this.container.utilities.discord.sendEphemeralMessage(message, {
-          content:
-            `Algo aconteceu com os dados do usuário, ele foi resetado para o cargo VINCULADO no Banco de Dados e atribuido os respectivos cargos.`,
+          content: `Algo aconteceu com os dados do usuário, ele foi resetado para o cargo VINCULADO no Banco de Dados e atribuido os respectivos cargos.`,
           method: "reply",
         });
       }
-		}
+    }
 
-		await cachedGuild.members
-			.edit(member, {
-				nick: `· ${profile.name}`,
-			})
-			.catch(() => {
-				this.container.logger.warn(
-					"[LinkCommand#messageRun] Failed to edit user nick.",
-				);
-			});
+    await cachedGuild.members
+      .edit(member, {
+        nick: `· ${profile.name}`,
+      })
+      .catch(() => {
+        this.container.logger.warn(
+          "[LinkCommand#messageRun] Failed to edit user nick."
+        );
+      });
 
     if (existingUser?.discordId === "0") {
-      if (!existingUser.latestPromotionJobId || !existingUser.latestPromotionRoleId) {
+      if (
+        !existingUser.latestPromotionJobId ||
+        !existingUser.latestPromotionRoleId
+      ) {
         await this.container.utilities.discord.sendEphemeralMessage(message, {
-          content:
-            `Ocorreu um erro inesperado, contate o Desenvolvedor.`,
+          content: `Ocorreu um erro inesperado, contate o Desenvolvedor.`,
           method: "reply",
         });
 
@@ -294,73 +305,74 @@ export class LinkCommand extends Command {
       }
 
       await this.container.prisma.user
-      .update({
-        where: { habboId: existingUser.habboId },
-        data: { discordId: member.id },
-      })
-      .catch(() => undefined);
+        .update({
+          where: { habboId: existingUser.habboId },
+          data: { discordId: member.id },
+        })
+        .catch(() => undefined);
 
       await cachedGuild.members
-      .addRole({
-        user: member,
-        role: existingUser.latestPromotionJobId,
-      })
-      .catch(() => undefined);
+        .addRole({
+          user: member,
+          role: existingUser.latestPromotionJobId,
+        })
+        .catch(() => undefined);
       await cachedGuild.members
-      .addRole({
-        user: member,
-        role: existingUser.latestPromotionRoleId,
-      })
-      .catch(() => undefined);
-		} else if (existingUser) {
-			await this.container.prisma.user
-				.update({
-					where: { discordId: existingUser.discordId },
-					data: { habboId: profile.uniqueId, habboName: profile.name },
-				})
-				.catch(() => undefined);
-		} else {
-			await this.container.prisma.user
-				.create({
-					data: {
+        .addRole({
+          user: member,
+          role: existingUser.latestPromotionRoleId,
+        })
+        .catch(() => undefined);
+    } else if (existingUser) {
+      await this.container.prisma.user
+        .update({
+          where: { discordId: existingUser.discordId },
+          data: { habboId: profile.uniqueId, habboName: profile.name },
+        })
+        .catch(() => undefined);
+    } else {
+      await this.container.prisma.user
+        .create({
+          data: {
             habboId: profile.uniqueId,
             discordId: member.id,
             habboName: profile.name,
             latestPromotionDate: new Date(),
             latestPromotionRoleId: ENVIRONMENT.SECTORS_ROLES.INICIAL.id,
-            latestPromotionJobId: ENVIRONMENT.JOBS_ROLES.VINCULADO.id },
-				})
-				.catch(() => undefined);
-		}
+            latestPromotionJobId: ENVIRONMENT.JOBS_ROLES.VINCULADO.id,
+          },
+        })
+        .catch(() => undefined);
+    }
 
-		const notificationChannel = await member.guild.channels.fetch(
-			ENVIRONMENT.NOTIFICATION_CHANNELS.HABBO_USERNAME_ADDED,
-		);
+    const notificationChannel = await member.guild.channels.fetch(
+      ENVIRONMENT.NOTIFICATION_CHANNELS.HABBO_USERNAME_ADDED
+    );
 
-		if (!notificationChannel?.isTextBased()) {
-			throw new Error("Can't send message to non-text channel.");
-		}
+    if (!notificationChannel?.isTextBased()) {
+      throw new Error("Can't send message to non-text channel.");
+    }
 
-		const embed = new EmbedBuilder()
-			.setColor(EmbedColors.Default)
-			.setAuthor({
-				name: `${existingUser ? "Revinculado" : "Vinculando"} por @${
-					message.author.tag
-				}`,
-				iconURL: message.author.displayAvatarURL(),
-			})
-			.addFields([
-				{ name: "Habbo", value: profile.name, inline: true },
-				{ name: "Discord", value: `<@${member.id}>`, inline: true },
-			])
-			.setThumbnail(
-				`https://www.habbo.com/habbo-imaging/avatarimage?figure=${profile.figureString}&size=b&gesture=std`,
-			);
+    const embed = new EmbedBuilder()
+      .setColor(EmbedColors.Default)
+      .setAuthor({
+        name: `${existingUser ? "Revinculado" : "Vinculando"} por @${
+          message.author.tag
+        }`,
+        iconURL: message.author.displayAvatarURL(),
+      })
+      .addFields([
+        { name: "Habbo", value: profile.name, inline: true },
+        { name: "Discord", value: `<@${member.id}>`, inline: true },
+      ])
+      .setThumbnail(
+        `https://www.habbo.com/habbo-imaging/avatarimage?figure=${profile.figureString}&size=b&gesture=std`
+      );
 
-		await notificationChannel.send({
-			embeds: [embed],
-		});
+    await notificationChannel.send({
+      embeds: [embed],
+    });
 
-		await message.react("✅");
-	}
+    await message.react("✅");
+  }
 }
