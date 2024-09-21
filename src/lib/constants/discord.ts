de5d2dd6ea8956225c274825bdb9c4a899e40439
cauemondek <cauemondekk@gmail.com>
2024-09-21 18:15:37 -0300

    :bricks: ci: Change colors constants for Embeds

--- src/lib/constants/discord.ts
+++ src/lib/constants/discord.ts
@@ -4,4 +4,11 @@ export enum EmbedColors {
 	Error = 0xda373c,
 	Info = 0x375de3,
 	Warn = 0xe6b800,
+
+  LalaRed = 0xb90f0f,
+  Hire = 0x004bad,
+  Diary = 0x6c6a6a,
+  Alert = 0xe3e300,
+  AddAmount = 0x147201,
+  RemoveAmount = 0x972b00
 }
