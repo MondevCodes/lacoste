41dfb0fdd538f54889f8a56e885377934eb29381
cauemondek <cauemondekk@gmail.com>
2024-10-21 05:36:59 -0300

    :test_tube: bug: Add id

--- src/modules/core/commands/pingBot.ts
+++ src/modules/core/commands/pingBot.ts
@@ -29,7 +29,7 @@ export class pingBot extends Command {
         new EmbedBuilder()
           .setTitle("Latência do Bot 🤖")
           .setDescription(
-            `A latência do Bot está **${latencyStatus}** com ping de **${botPing}ms**\n ${ENVIRONMENT.JOBS_ROLES.ADMINISTRADOR}`
+            `A latência do Bot está **${latencyStatus}** com ping de **${botPing}ms**\n ${ENVIRONMENT.JOBS_ROLES.ADMINISTRADOR.id}`
           )
           .setColor(embedColor),
       ],
