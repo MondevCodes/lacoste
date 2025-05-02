52b5b3c3011e2733491a795f43579cd83fdacf3e
pedromondek <phmondek@gmail.com>
2025-05-01 21:58:59 -0300

    :truck: Modified Images

--- src/modules/core/commands/send.ts
+++ src/modules/core/commands/send.ts
@@ -11,6 +11,7 @@ import { ApplyOptions } from "@sapphire/decorators";
 
 import { FormIds } from "$lib/constants/forms";
 import { EmbedColors } from "$lib/constants/discord";
+import { ImgThumbnail } from "$lib/constants/imgs-thumbnail";
 
 import { encodeButtonId as encodeFireButtonId } from "../../ticket/interactions/fire";
 import { encodeButtonId as encodeHireButtonId } from "../../ticket/interactions/hire";
@@ -78,9 +79,7 @@ export default class SendCommand extends Command {
             .setDescription(
               '**Controle Financeiro**\n Para adicionar ou remover de membro específico ou de um grupo (ex. setor estagiário), basta clicar no botão correspondente. Em adição ou remoção em grupo, use vírgula (",") para separar os nicks (ex: Brendo, Fortissima).\n\n **Medalhas**\n Siga a função correspondente.'
             )
-            .setImage(
-              "https://cdn.discordapp.com/attachments/1319339476396544142/1319344228177940490/Lac._6.gif?ex=67659e90&is=67644d10&hm=b52904d512022d43d725ee4f02565c8ba535e84917b5fcb9caad65c8e7c92786&"
-            ),
+            .setImage(ImgThumbnail.Fundacao),
         ],
         components: [
           new ActionRowBuilder<ButtonBuilder>().addComponents(
@@ -155,9 +154,7 @@ export default class SendCommand extends Command {
             .setDescription(
               "Selecione o tipo de formulário que deseja e responda o questionário que será aberto. Ao finalizar, seu formulário será enviado para a equipe de avaliação."
             )
-            .setImage(
-              "https://cdn.discordapp.com/attachments/1319339476396544142/1319341765286953000/Lac..gif?ex=67659c45&is=67644ac5&hm=791f4b4f551abd3bd39d5e24ba48bcd439acee743a98e1c8e84bfa720757cef0&"
-            ),
+            .setImage(ImgThumbnail.Avaliativo),
         ],
         components: [
           new ActionRowBuilder<ButtonBuilder>().addComponents(
@@ -184,9 +181,7 @@ export default class SendCommand extends Command {
             .setDescription(
               "Clique no botão abaixo para abrir o questionário que, ao finalizar, será enviado para o canal de sugestões ou reclamações."
             )
-            .setImage(
-              "https://cdn.discordapp.com/attachments/1319339476396544142/1319343804477870163/Lac._5.gif?ex=67659e2b&is=67644cab&hm=849ef1894bb0a99a072fbaa7fe40d49b0b633bcc13d52680d92a53d585255e0e&"
-            ),
+            .setImage(ImgThumbnail.Diretoria),
         ],
         components: [
           new ActionRowBuilder<ButtonBuilder>().addComponents(
@@ -213,9 +208,7 @@ export default class SendCommand extends Command {
             .setDescription(
               "Selecione o tipo de formulário que deseja e responda o questionário que será aberto. Ao finalizar, seu formulário será enviado para o canal de relatórios."
             )
-            .setImage(
-              "https://cdn.discordapp.com/attachments/1319339476396544142/1319342114156449965/Lac._1.gif?ex=67659c98&is=67644b18&hm=1ad57ef9c54311d507f6d4c0ac849a16516e09b3b288e0d1deaf9e1af25e6d08&"
-            ),
+            .setImage(ImgThumbnail.Organizacional),
         ],
         components: [
           new ActionRowBuilder<ButtonBuilder>().addComponents(
@@ -242,9 +235,7 @@ export default class SendCommand extends Command {
             .setDescription(
               "Considere nossas normas atuais antes de aplicar uma função.\nFique atento ao canal que será gerado e as perguntas feitas pelo BOT, em seguida o registro da mesma será publicado. \n\nClique no botão que corresponda à ação desejada."
             )
-            .setImage(
-              "https://cdn.discordapp.com/attachments/1319339476396544142/1319343339732205608/Lac._4.gif?ex=67659dbc&is=67644c3c&hm=9765bfdda0643080f387a36541d9616c6d61f00bf6b827aef7e31fb958c01dab&"
-            ),
+            .setImage(ImgThumbnail.Administrativo),
         ],
         components: [
           new ActionRowBuilder<ButtonBuilder>().addComponents(
@@ -305,9 +296,7 @@ export default class SendCommand extends Command {
             .setDescription(
               "Para enviar sugestões, reclamações, dúvidas ou solicitar medalhas aperte no Ticket! Um administrador irá responder o mais rápido possível. Fique a vontade para enviar um elogio também."
             )
-            .setImage(
-              "https://cdn.discordapp.com/attachments/1319339476396544142/1319341196686000260/LacOUV_8.gif?ex=67659bbd&is=67644a3d&hm=45df3f1b5c6f4bb5a456f2b3e4dc5d1cac3e0170bf24f420438cc7a990e760f5&"
-            ),
+            .setImage(ImgThumbnail.Ouvidoria),
         ],
         components: [
           new ActionRowBuilder<ButtonBuilder>().addComponents(
@@ -334,9 +323,7 @@ export default class SendCommand extends Command {
             .setDescription(
               "Escolha o tipo de ação que deseja fazer o clique no botão abaixo, preencha os formulários corretamente e aguarde a aprovação do seu pedido."
             )
-            .setImage(
-              "https://cdn.discordapp.com/attachments/1319339476396544142/1319342913485799576/Lac._3.gif?ex=67659d56&is=67644bd6&hm=0d6b178590d4f8b3620726291448249d27b5444010a521f800a9842783604540&"
-            ),
+            .setImage(ImgThumbnail.Promocional),
         ],
         components: [
           new ActionRowBuilder<ButtonBuilder>().addComponents(
@@ -363,9 +350,7 @@ export default class SendCommand extends Command {
             .setDescription(
               "**Alteração de Nickname**\nMudou seu apelido no Habbo? Clique no botão 'Renomear' para automaticamente atualizar o seu perfil.\n\n**Alteração de Conta do Habbo**\nMudou de conta no Habbo? Clique no botão 'Trocar conta do Habbo' para seus dados serem atualizados.\nAguarde a aprovação.\n\n**Alteração de Conta do Discord**\nMudou de conta no discord? Clique no botão 'Trocar conta do discord' para seus dados serem atualizados.\nAguarde a aprovação."
             )
-            .setImage(
-              "https://cdn.discordapp.com/attachments/1319339476396544142/1319340531310133299/LacOUV_7.gif?ex=67659b1e&is=6764499e&hm=c8c838715e8eb604c928733a3d23be3c740de65a521de599815856e739e963d3&"
-            ),
+            .setImage(ImgThumbnail.MudarConta),
         ],
         components: [
           new ActionRowBuilder<ButtonBuilder>().addComponents(
